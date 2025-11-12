c77dd978d0191320ca8da3a8e25319ced65d7c09
mthsneves <mthsnevesdev@outlook.com>
2025-11-11 21:27:25 -0300

    Ocultado o Bearer token do Script para melhor segurança

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,15 +1,13 @@
-describe('Api Adopet', () =>{
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5MWI4OGZkMy01MDA2LTRkZWMtYjg1NC1iMjg1ZTEwMWQ5Y2UiLCJhZG9wdGVyTmFtZSI6IlRlc3RlIGRlIHRlc3RlIiwiaWF0IjoxNzYyNjIzOTcwLCJleHAiOjE3NjI4ODMxNzB9.iSSBPRzN3SGedJlPVbquHw9-nBH-UjYjY0_MTc1nt3A'
-
-    it('Mensagens da API', () =>{
-        cy.request({
-            method: 'GET' ,
-            url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-777112-415b-95d2-07904b0d1a1c',
-            headers: { authorization }
-        }).then((res)=>{
-            expect(res.status).to.be.equal(200)
-            expect(res.body).is.not.empty
-            expect(res.body).to.have.property('msg')
-        })
+describe('Api Adopet', () => {
+  it('Mensagens da API', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-777112-415b-95d2-07904b0d1a1c',
+      headers: Cypress.env() 
+    }).then((res) => {
+      expect(res.status).to.eq(200)
+      expect(res.body).to.not.be.empty
+      expect(res.body).to.have.property('msg')
     })
+  })
 })
